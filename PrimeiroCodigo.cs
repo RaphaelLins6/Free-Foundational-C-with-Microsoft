07bc567a32c6f2683ed84263230a00898edcbd2d
Raphael Lins <rapharell06@gmail.com>
2025-04-08 17:56:04 -0300

    Primeiro exercício.

---
+++ PrimeiroCodigo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Primeiro exercício de C#
+// Criar um programa que imprima na tela a frase "Olá mundo!".
+class PrimeiroCodigo {
+    static void Main(string[] args) {
+        Console.WriteLine("Olá mundo!");
+
+        // Adicione novas linhas de código aqui:
+        Console.Write("Parabéns!");
+        Console.Write(" ");
+        Console.Write("Você escreveu suas primeiras linhas de código em C#.");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Aperte enter para continuar...");
+        Console.ReadLine();
+    }
+}
+
